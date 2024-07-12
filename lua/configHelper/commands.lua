41da2fa3af84147f8da5f636d52565fec8192e20
notanoty <timariefev05@gmail.com>
2024-07-12 11:25:09 +0200

    add file path checker and support of multiple file symbols

--- lua/configHelper/commands.lua
+++ lua/configHelper/commands.lua
@@ -2,15 +2,6 @@ vim = vim -- I need to write this because my lsp is very annoying
 
 local core = require("core")
 
-vim.api.nvim_create_user_command("ShowTime", function()
-  core.show_time()
-end, {})
-vim.api.nvim_create_user_command("ShowRandomNumber", function()
-  core.show_randm_number()
-end, {})
-vim.api.nvim_create_user_command("PrintText", function()
-  core.print_text()
-end, {})
 vim.api.nvim_create_user_command("CreatTest", function()
   core.create_file_in_directory()
 end, {})
--- lua/configHelper/core.lua
+++ lua/configHelper/core.lua
@@ -1,25 +1,67 @@
 vim = vim
 
+local function get_full_word_under_cursor()
+  local line = vim.api.nvim_get_current_line()
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+  local cursor_col = cursor_pos[2] + 1
+
+  local word_start, word_end = cursor_col, cursor_col
+
+  while word_start > 1 and line:sub(word_start - 1, word_start - 1):match("[~$_%w./]") do
+    word_start = word_start - 1
+  end
+
+  while word_end <= #line and line:sub(word_end, word_end):match("[~$_%w./]") do
+    word_end = word_end + 1
+  end
+
+  return line:sub(word_start, word_end - 1)
+end
+
+
+
+local function is_path(path)
+  print("The path is: (" .. path .. ")")
+  if path == "" then return false
+  end
+  -- print("Path checker(case 1): " .. string.match(path, "[/%a+1]+"))
+  -- print("Path checker(case 2): " .. string.match(path, "[~/%a+][/%a]+"))
+  if path == string.match(path, "[/%a]+") then
+    return true
+  elseif path == string.match(path, "[~/%a+][/%a]*")  then
+    return true
+  elseif path == string.match(path, "[./%a+][/%a]*")  then
+    return true
+  end
+  return false
+end
+
+local function get_current_buffer_content()
+    local buf = vim.api.nvim_get_current_buf()
+    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+    return lines
+end
+
+local function search_word(lines, word)
+  local path = nil
+  for i = 1, #lines, 1 do
+    if string.match(lines[i], word) then
+      path = string.match(lines[i], word .." *= *(.*)")
+      break
+    end
+  end
+
+  if path ~= nil then
+    return path
+  else
+    print("Variable was not found")
+    return ""
+  end
+end
+
+
 local M = {}
-function M.hello_world_function()
-  print("Hello, World function!")
-end
 
-function M.show_time()
-  print("Current time: " .. os.date())
-end
-
-function M.show_randm_number()
-  print("Random number: " .. math.random(1, 100))
-end
-
-function M.print_text()
-  local text = vim.fn.input("Enter your text: ")
-
-  local cwd = vim.loop.cwd()
-  print("Current directory: " .. cwd)
-  print("Your text is: " .. text)
-end
 
 function M.create_file_in_directory()
   local cwd = vim.loop.cwd()
@@ -34,82 +76,35 @@ function M.create_file_in_directory()
   end
 end
 
-local function get_full_word_under_cursor()
-  local line = vim.api.nvim_get_current_line()
-  local cursor_pos = vim.api.nvim_win_get_cursor(0)
-  local cursor_col = cursor_pos[2] + 1
 
-  local word_start, word_end = cursor_col, cursor_col
-
-  while word_start > 1 and line:sub(word_start - 1, word_start - 1):match("[$_%w./]") do
-    word_start = word_start - 1
-  end
-
-  while word_end <= #line and line:sub(word_end, word_end):match("[$_%w./]") do
-    word_end = word_end + 1
-  end
-
-  return line:sub(word_start, word_end - 1)
-end
-
-
-
-local function is_path(word)
-  return true
-end
-
-local function get_current_buffer_content()
-    local buf = vim.api.nvim_get_current_buf()
-    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-    return lines
-end
-
-local function search_word(lines, word)
-  local path = nil
-  for i = 1, (#lines), 1 do
-    if string.match(lines[i], word) then
-      path = string.match(lines[i], word .." *= *(.*)")
-      break
-    end
-  end
-
-  if path ~= nil then 
-    return path
-  else
-    print("Variable was not found")
-    return ""
-  end 
-end
+-- ~/aaa
+-- /aaa/aaa//
+-- /test/test/a
 -- $test = ~/Desktop
 -- $test/programming/
 function M.go_by_path()
-  -- Get the current cursor position
   local path = get_full_word_under_cursor()
-  local word = ""
-  print("Path: (" .. path ..")")
+  local word =nil 
   word = string.match(path, "($%a+)")
-  -- print("Word: " .. word)
-  local need_search = true
+  local has_search = true
   if word == nil then
-    need_search = false 
+    has_search = false
   else
     path = string.gsub(path, word, "")
   end
 
-  if need_search then
-    print("Path: " .. path .. " Word: " .. word)
-  end
   if is_path(path) then
-    -- print("(" ..path .. ") is a path")
     local lines = get_current_buffer_content()
-    if need_search then
+    if has_search then
       local searched_word = search_word(lines, word)
+      print("Path (correct search_word): " .. searched_word .. path)
       vim.api.nvim_command("edit" .. searched_word .. path)
     else
+      print("Path (correct no search_word): " .. path)
       vim.api.nvim_command("edit" .. path)
     end
   else
-    print(path .. " is not a path")
+    print("Path (incoreect): "  .. path)
   end
 end
 
