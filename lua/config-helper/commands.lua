6872a44d08f018f29cb5db075c07924a5c3d9c12
notanoty <timariefev05@gmail.com>
2024-07-12 18:30:35 +0200

    try to fix the bug

--- lua/config-helper/commands.lua
+++ lua/config-helper/commands.lua
@@ -1,6 +1,6 @@
 vim = vim -- I need to write this because my lsp is very annoying
 
-local core = require("core")
+local core = require("config-helper.core")
 
 vim.api.nvim_create_user_command("CreatTest", function()
   core.create_file_in_directory()
--- lua/config-helper/init.lua
+++ lua/config-helper/init.lua
@@ -3,7 +3,6 @@ local M = {}
 
 function M.setup()
   -- Your plugin setup code here
-  vim.cmd('echo "config-helper is loaded"')
   -- Additional setup...
   require("config-helper.core")
 
